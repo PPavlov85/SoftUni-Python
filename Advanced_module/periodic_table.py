9c7fd1ee51dbca946a20e7fd60d58a6770fa5d27
Pavel Pavlov <gsxr_1000@abv.bg>
2020-05-31 21:22:31 +0300

    Delete periodic_table.py

--- Advanced_module/periodic_table.py
+++
@@ -1,9 +0,0 @@
-n = int(input())
-unique = set()
-
-for _ in range(n):
-    compounds = [x for x in input().split()]
-    for element in compounds:
-        unique.add(element)
-
-[print(x) for x in unique]
